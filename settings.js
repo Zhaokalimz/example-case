51c990a2088bc29dc868c0a1b09e26c52e41aa9e
Zhaokalimz <150653174+Zhaokalimz@users.noreply.github.com>
2023-11-15 19:21:00 +0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -10,16 +10,16 @@ global.APIKeys = {
 	'https://api.zeeoneofc.my.id': 'Your Key',
 }
 
-global.namabot = "Bot Store"
-global.namaowner = "Zeeoneofc"
-global.packname = namabot
-global.author = namaowner
+global.namabot = "Vill么in-Md"
+global.namaowner = "Lukhaz Vill么in"
+global.packname = Vill么in-Md
+global.author = Lukhaz Vill么in
 global.myweb = "https://s.id/zeeoneofc"
-global.footer_text = "© " + namabot
-global.pp_bot = fs.readFileSync("./image/foto.jpg")
-global.qris = fs.readFileSync("./image/qris.jpg")
-global.owner = ['62887435047326']
-global.pairingNumber = "" //pairing number to install bot 
+global.footer_text = "© " + Vill么in-Md
+global.pp_bot = fs.readFileSync("")
+global.qris = fs.readFileSync("")
+global.owner = ['6283822459641']
+global.pairingNumber = "6283822459641" //pairing number to install bot 
 global.sessionName = 'session'
 global.background = "https://telegra.ph/file/40342e0085d521cb8b907.jpg" //maks size 50kb agar welcome nya gk lemot
 global.prefa = ['-_-']
